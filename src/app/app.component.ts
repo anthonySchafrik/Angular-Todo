689dfc1e8c9fe99829ed4402d63b25436a91d7dd
Anthony Schafrik <anthonyschafrik@gmail.com>
2018-12-14 22:58:07 -0500

    Finsh the logic for app no styling done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-todo';
+  todo = '';
+  todos = [];
+
+  onAddTodo() {
+    const { todo, todos } = this;
+
+    todos.push(todo);
+
+    this.todo = ''
+  }
+
+  onDeleteTodo(e) {
+    const { todos } = this;
+ 
+    let todo:string = e.target.parentElement.childNodes[0].data.toString().trim();
+    let item = todos.indexOf(todo);
+
+    todos.splice(item, 1);
+  }
 }
